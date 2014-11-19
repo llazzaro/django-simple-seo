5980df0596693251a389a1b5097fbe5537ababf1
Agustin Carrasco <asermax@gmail.com>
2014-11-19 10:40:18 -0300

    setup: bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-simple-seo',
-    version='2.0.2',
+    version='2.0.4',
     packages=['simple_seo', 'simple_seo.templatetags'],
     package_data={'simple_seo': ['templates/admin/simple_seo/*.html']},
     include_package_data=True,
